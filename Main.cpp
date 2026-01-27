7279b58c12af96e4b781d57a39271b50fc9f0542
AvielleGregorio <sophia.gregorio@student.ateneo.edu>
2026-01-28 00:25:40 +0800

    Added Selection and Fixed Camera Behavior

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,12 @@
 #include <raylib.h>
 #include <raymath.h>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 const float WINDOW_WIDTH(800);
 const float WINDOW_HEIGHT(600);
@@ -9,84 +16,168 @@ Vector2 maxEdge = {1548, 1032};
 
 bool isStatic = false;
 
+struct Object {
+    string name;
+    int x;
+    int y;
+};
+
 int main() {
     SetConfigFlags(FLAG_WINDOW_HIGHDPI);
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "AlvarezCorpuzGregorio_Homework01");
-
-    Texture2D background = LoadTexture("ispy.jpeg");
     
     Vector2 position = {400, 300};
 
-    Rectangle view = {position.x-250, position.y-150, 500, 300};
+    Rectangle view = {0, 0, 500, 300};
 
     SetTargetFPS(60.0f);
-
 
     int cam_type = 0;
     Camera2D camera = {0};
     camera.target = position;
     camera.offset = {WINDOW_WIDTH/2, WINDOW_HEIGHT/2};
-    camera.zoom = 0.5f;
+
+    bool selecting = false;     
+    float selectSize = 100.0f;
+
+    //for pointer
+    float center_x = WINDOW_WIDTH / 2.0f;
+    float center_y = WINDOW_HEIGHT / 2.0f;
+    float pointerSize = 20.0f;
+
+    //Setting up using the settings.txt
+    ifstream file("settings.txt");
+    string line;
+    string imageName;
+    vector<Object> objects;
+
+    while(getline(file, line)){
+        istringstream stream(line); //https://www.geeksforgeeks.org/cpp/processing-strings-using-stdistringstream/
+        string key;
+        stream >> key; //this reads the first tokens, puts them in key so we can identify the type
+
+        if(key == "IMAGE_NAME"){
+            stream >> imageName; 
+        }else if(key.rfind("OBJECT", 0) == 0){//https://cplusplus.com/reference/string/string/rfind/
+            string objectName;
+            int x, y;
+            stream >> objectName >> x >> y;
+            objects.push_back({objectName, x, y});
+        }else if(key == "CAM_EDGES"){
+            int minEdge_x, minEdge_y, maxEdge_x, maxEdge_y;
+            stream >> minEdge_x >> minEdge_y >> maxEdge_x >> maxEdge_y;
+            minEdge.x = minEdge_x;
+            minEdge.y = minEdge_y;
+            maxEdge.x = maxEdge_x;
+            maxEdge.y = maxEdge_y;
+        } else if (key == "CAM_WINDOW") { //NOT USED YET
+            int w, h;
+            stream >> w >> h;
+        } else if (key == "CAM_DRIFT") { //NOT USED YET
+            float drift;
+            stream >> drift;
+        } else if (key == "CAM_ZOOM") {
+            float zoom;
+            stream >> zoom;
+            camera.zoom = zoom;
+        }
+
+    }
+
+    file.close();
+    Texture2D background = LoadTexture(imageName.c_str());
 
     while (!WindowShouldClose()) {
         float delta_time = GetFrameTime();
-        
-        if(IsKeyPressed(KEY_ENTER) && camera.zoom == 0.5f) {
-            camera.zoom = 1.0f;
-            isStatic = true;
+        if (IsKeyPressed(KEY_ENTER)) {
+            if (!isStatic) {
+                // First enter: zoom in
+                camera.zoom = 1.0f;
+                isStatic = true;
+                cout << "Zoomed in" << endl;
+            } else if (!selecting) {
+                // Second enter: selection time
+                selecting = true;
+                cout << "secod enter for select" << endl;
+                for (auto& obj : objects) {
+                    cout << obj.x << " " << obj.y << endl;
+                    if(
+                        (position.x > obj.x) &&
+                        (position.x < (obj.x + selectSize)) &&
+                        (position.y > obj.y) &&
+                        (position.y < (obj.y + selectSize))
+                    ){
+                        cout << "Successfully selected " << obj.name << endl;
+                    }
+                }
+            }
         }
         
         if(IsKeyPressed(KEY_TAB) && camera.zoom == 1.0f) {
             camera.zoom = 0.5f;
             isStatic = false;
+            selecting = false;
         }
 
         if (!isStatic) {
             if(IsKeyDown(KEY_W)) {
                 position.y -= 200 * delta_time;
-                view.y -= 200 * delta_time;
             }
             if(IsKeyDown(KEY_A)) {
                 position.x -= 200 * delta_time;
-                view.x -= 200 * delta_time;
             }
             if(IsKeyDown(KEY_S)) {
                 position.y += 200 * delta_time;
-                view.y += 200 * delta_time;
             }
             if(IsKeyDown(KEY_D)) {
                 position.x += 200 * delta_time;
-                view.x += 200 * delta_time;
             }
         }
 
+        //comment or uncomment for debugging to find the position of the stuff since this just prints the positions in the console
+        //cout << position.x << " " << position.y << endl;
+
         position = Vector2Clamp(position, {minEdge.x + 10, minEdge.y + 10}, {maxEdge.x - 10, maxEdge.y - 10});
-        view.x = Clamp(view.x, minEdge.x, maxEdge.x - view.width);
-        view.y = Clamp(view.y, minEdge.y, maxEdge.y - view.height);
+        position = Vector2Clamp(position, {minEdge.x + 10, minEdge.y + 10}, {maxEdge.x - 10, maxEdge.y - 10});
+        
+        view.x = camera.target.x - view.width / 2;
+        view.y = camera.target.y - view.height / 2;
 
-        camera.target = position;
+        if (position.x < view.x) camera.target.x -= view.x - position.x;
+        else if (position.x > view.x + view.width) camera.target.x += position.x - (view.x + view.width);
 
-        Vector2 camTarget = position;
+        if (position.y < view.y) camera.target.y -= view.y - position.y;
+        else if (position.y > view.y + view.height) camera.target.y += position.y - (view.y + view.height);
 
-        camTarget = Vector2Clamp(camTarget, {minEdge.x + ((WINDOW_WIDTH/2) / camera.zoom), minEdge.y + ((WINDOW_HEIGHT/2) / camera.zoom)}, {maxEdge.x - ((WINDOW_WIDTH/2) / camera.zoom), maxEdge.y - ((WINDOW_HEIGHT/2) / camera.zoom)});
-
-        camera.target = camTarget;
+        camera.target = Vector2Clamp(camera.target,
+            {minEdge.x + (WINDOW_WIDTH/2)/camera.zoom, minEdge.y + (WINDOW_HEIGHT/2)/camera.zoom},
+            {maxEdge.x - (WINDOW_WIDTH/2)/camera.zoom, maxEdge.y - (WINDOW_HEIGHT/2)/camera.zoom});
 
         BeginDrawing();
         BeginMode2D(camera);
         ClearBackground(MAROON);
         DrawTexture(background, minEdge.x, minEdge.y, WHITE);
         DrawRectangleLinesEx(view, 10.0f, SKYBLUE);
-        // DrawCircle(position.x, position.y, 10.0f, BLUE);
+        DrawCircle(position.x, position.y, 10.0f, BLUE);
+
+
+        //comment or uncomment for debugging, this will add a green box so you can see the selection area
+        for (auto& obj : objects) {
+            DrawRectangle(obj.x, obj.y, selectSize, selectSize, GREEN);
+            DrawText(obj.name.c_str(), obj.x, obj.y - 20, 20, GREEN);
+        }
+
         EndMode2D();
-
         EndDrawing();
-
     }
+
     UnloadTexture(background);
     CloseWindow();
     return 0;
+
 }
 
 
 // DELETE BEFORE SUBMISSION: clang++ Main.cpp libraylib.a -std=c++17 \-framework Cocoa -framework IOKit -framework CoreVideo -framework OpenGL -framework Foundation -o ispy
+//  C:\raylib\w64devkit\w64devkit.exe
+// HII SIRRR!! - Avielle: cd Documents/"[Y4] Second Semester 2026"/GDEV42/GDEV42 || g++ Main.cpp -o out -I raylib/ -L raylib/ -lraylib -lopengl32 -lgdi32 -lwinmm
